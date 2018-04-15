2ac77d1cf49959715b8b213bca5841c6dd1155f0
lptree <lphnl1@163.com>
2018-04-16 07:26:38 +0800

    init-with mongo loog

---
+++ src/main/java/com/lptree/utils/TestUtils.java
@@ -0,0 +1,12 @@
+package com.lptree.utils;
+
+/**
+ * @Description:
+ * @Author: lptree
+ * @Date: Created in 2018/4/15 19:02
+ */
+public class TestUtils {
+    public static void HelloJars() {
+        System.out.println("Hello Jars!");
+    }
+}
